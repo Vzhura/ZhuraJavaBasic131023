8ef4e7800b6b2b4dcc495c21248d881aef512d13
User <slaven90@ukr.net>
2023-10-13 22:13:07 +0300

    Second commit for HW

--- src/FirstProgramHomeWork.java
+++ src/FirstProgramHomeWork.java
@@ -1,5 +1,6 @@
 public class FirstProgramHomeWork {
     public static void main(String[] args) {
         System.out.println("Hello world and Git! I am Slava");
+        System.out.println("Phrase for Second commit");
     }
 }
